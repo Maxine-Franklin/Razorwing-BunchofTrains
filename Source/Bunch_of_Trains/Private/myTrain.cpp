5950eb8d92af025ca9a2c314954f5409d565ad1a
Maxine Franklin <GitHub@oscarfranklin.co.uk>
2022-03-05 14:43:56 +0000

    Train logic v0.02.2
    Introduced a new actor script to handle individual trains

---
+++ Source/Bunch_of_Trains/Private/myTrain.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "myTrain.h"
+
+// Sets default values
+AmyTrain::AmyTrain()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AmyTrain::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AmyTrain::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
