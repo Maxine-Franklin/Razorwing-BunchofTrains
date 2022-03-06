6a20945a32ae644e5ae1ab5414c6117fe3fa8db1
Maxine Franklin <GitHub@oscarfranklin.co.uk>
2022-03-06 15:56:49 +0000

    Train Logic v0.06
    Setup trainMaster to be an actor

--- Source/Bunch_of_Trains/Private/trainMaster.cpp
+++ Source/Bunch_of_Trains/Private/trainMaster.cpp
@@ -9,6 +9,21 @@ AtrainMaster::AtrainMaster()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	myMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("trainMesh"));
+	myMesh->SetupAttachment(RootComponent);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> myMeshFinder(TEXT("/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube")); //Obtains the static mesh from the content browser
+	if (myMeshFinder.Succeeded()) //If mesh exists, then...
+	{
+		myMesh->SetStaticMesh(myMeshFinder.Object); //Applies mesh
+		//Adjusts mesh properties
+		myMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+		myMesh->SetWorldScale3D(FVector(1.0f, 22.75f, 17.75f));
+		myMesh->SetSimulatePhysics(true);
+		myMesh->SetEnableGravity(false);
+		myMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	}
+
 }
 
 // Called when the game starts or when spawned
@@ -27,5 +42,15 @@ void AtrainMaster::Tick(float DeltaTime)
 
 void AtrainMaster::SpawnTrain(FTransform Trans, float Velocity, bool Active)
 {
-
+	FVector Location = GetActorLocation();
+	switch (abs(forwardDirection))
+	{
+	case 1:
+		Location.X = Player->GetActorLocation().X;
+	case 2:
+		Location.Y = Player->GetActorLocation().Y;
+	case 3:
+		Location.Z = Player->GetActorLocation().Z;
+	}
+	SetActorLocation(Location);
 }
